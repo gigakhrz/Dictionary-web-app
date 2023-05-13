a97ea3cfef54faf76fdb3c314b94340d996a9e10
gigakhrz <kharazishvilig13@gmail.com>
2023-05-13 12:19:40 +0400

    update fontlist background color

--- src/components/FontList.tsx
+++ src/components/FontList.tsx
@@ -33,8 +33,10 @@ const FontList = ({
         className={` ${
           chooseFont ? "flex" : "hidden"
         } flex-col items-start p-6 gap-[16px] w-[183px] h-[152px] rounded-[16px] ${
-          check ? "shadow-colorwhite" : "shadow-colorblack"
-        } absolute right-0 top-[40px] bg-white`}
+          check
+            ? "shadow-colorwhite bg-white"
+            : "shadow-colorblack bg-[#1F1F1F]"
+        } absolute right-0 top-[40px] `}
       >
         <h5
           onClick={() => {
